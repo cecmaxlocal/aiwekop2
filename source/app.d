153360c372848de2710935c4eff32fc95bbce15c
raylivesun <162396999+raylivesun@users.noreply.github.com>
2025-06-24 12:57:32 -0300

    the source

--- source/app.d
+++ source/app.d
@@ -1,8 +1,9 @@
 import std.stdio;
-import aiwekop2; // Or the specific module you need
 
-void main() {
-    // Example of how to use your library's functions
-    auto result = someFunctionFromYourLib();
-    writeln("The result is: ", result);
+int desktop() {
+    return 0;
+}
+
+int main() {
+  	return desktop;
 }
